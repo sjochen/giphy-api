6d2dcc51374e43d990326cfeced64af0e44ecc2d
Stephen Jochen <stephenjochen2@gmail.com>
2019-06-25 17:12:05 -0400

    made it so the gifs load next to each other and all will load on one click

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,90 +3,87 @@ var topics = ["Batman", "Superman", "Green Arrow", "The Hulk", "Spiderman"];
 
 function renderButtons() {
 
-  
+
     $("#buttons").empty();
 
-    
+
     for (var i = 0; i < topics.length; i++) {
 
-        
+
         var a = $("<button>");
-        
+
         a.addClass("supbttn");
-        
+
         a.attr("data-name", topics[i]);
-        
+
         a.text(topics[i]);
-        
+
         $("#buttons").append(a);
     }
-}
-
-
-$("#addSup").on("click", function (event) {
-    event.preventDefault();
-    
-    var supher = $("#supInput").val().trim();
-
-   
-    topics.push(supher);
-
-    
-    renderButtons();
-});
-
-$(document).on("click", ".supbttn", displayHero);
-
-
-
-renderButtons();
-
-
-function displayHero(){
     $("button").on("click", function () {
-        
+
         var person = $(this).attr("data-name");
 
-        
+
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             person + "&api_key=aaYKdOTvastDy6M8HNdeNcBnSEZfYeCn&limit=10";
 
-        
+
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-                
-                var results = response.data;
-                console.log(results);
-                
-                for (var i = 0; i < results.length; i++) {
 
-                   
-                    var gifDiv = $("<div>");
+            var results = response.data;
+            console.log(results);
 
-                    
-                    var rating = results[i].rating;
+            for (var i = 0; i < results.length; i++) {
 
-                    
-                    var p = $("<p>").text("Rating: " + rating);
 
-                    
-                    var personImage = $("<img>");
+                var gifDiv = $("<div id='size'>");
 
-                   
-                    personImage.attr("src", results[i].images.fixed_height.url);
 
-                   
-                    gifDiv.append(p);
-                    gifDiv.append(personImage);
+                var rating = results[i].rating;
 
-                    
-                    $("#gifs-appear-here").prepend(gifDiv);
-                }
 
-            });
+                var p = $("<p>").text("Rating: " + rating);
+
+
+                var personImage = $("<img>");
+
+
+                personImage.attr("src", results[i].images.fixed_height.url);
+
+
+                gifDiv.append(p);
+                gifDiv.append(personImage);
+
+
+                $("#gifs-appear-here").prepend(gifDiv);
+            }
+        });
     });
+}
 
-};
+    
+            $("#addSup").on("click", function (event) {
+                event.preventDefault();
 
+                var supher = $("#supInput").val().trim();
+
+
+                topics.push(supher);
+
+
+                renderButtons();
+            });
+
+            
+        
+
+
+            renderButtons();
+
+
+
+        
